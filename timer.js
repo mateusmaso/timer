66f9e867e9931d739747146dcb18989082051479
Mateus Maso <m.maso25@gmail.com>
2013-07-24 21:36:41 -0700

    Created timer.js

---
+++ timer.js
@@ -0,0 +1,32 @@
+---
+published: false
+---
+
+module.exports = function() {
+
+  var _this = this;
+  this.seconds = 0;
+
+  this.start = function() {
+  	_this.stop();
+  	_this.resume();
+  };
+
+  this.stop = function() {
+  	_this.pause();
+  	_this.seconds = 0;
+  };
+
+  this.pause = function() {
+    clearInterval(_this.interval);
+    delete _this.interval;
+  };
+
+  this.resume = function() {
+    if (_this.interval) return;
+  	_this.interval = setInterval(function() {
+  	  _this.seconds += 1;
+  	}, 1000);
+  };
+  
+};
